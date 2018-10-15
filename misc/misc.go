af81ec1b863a1f8a0f93dac44d29f89ccfe3e532
Ilya Filippov <89099109104@mail.ru>
2018-10-15 13:30:33 +0300

    Add Filter function

--- misc/misc.go
+++ misc/misc.go
@@ -5,6 +5,26 @@ import (
 	"os"
 )
 
+// Filter filters an array of strings
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
+
+// RemoveEmpty is a predicate function for use in Filter to remove empty strings from array
+func RemoveEmpty(s string) bool {
+	if s == "" {
+		return false
+	}
+
+	return true
+}
+
 // HandleError prints error message
 func HandleError(err error, msg string) {
 	if err != nil {
