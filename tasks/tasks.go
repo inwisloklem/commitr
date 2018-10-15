b2b3c605576a8406510000ef64bbe36804c6ecb2
Ilya Filippov <89099109104@mail.ru>
2018-10-15 13:33:07 +0300

    Make Ask ask again if answer is empty line (required argument)

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -5,16 +5,24 @@ import (
 	"fmt"
 	"os/exec"
 	"regexp"
+	"strings"
 
 	"commitr/misc"
 )
 
 // Ask reads from command line
-func Ask(q string, reader *bufio.Reader) string {
+func Ask(q string, reader *bufio.Reader, required bool) string {
 	fmt.Print(q)
-	s, _ := reader.ReadString('\n')
 
-	return regexp.MustCompile(`\r|\n|^\s+|\s+$`).ReplaceAllString(s, "")
+	s, _ := reader.ReadString('\n')
+	res := regexp.MustCompile(`\r|\n|^\s+|\s+$`).ReplaceAllString(s, "")
+
+	if required == true && res == "" {
+		fmt.Println("This field can't be blank. Please try again.")
+		Ask(q, reader, required)
+	}
+
+	return res
 }
 
 // ExecuteSync synchronously executes command
@@ -45,4 +53,12 @@ func ExecCommands(cmds []string) {
 	for _, cmd := range cmds {
 		ExecuteSync(cmd)
 	}
+}
+
+// LoadCommands loads a list of OS commands from file
+func LoadCommands(fs, m, c string) []string {
+	re := strings.NewReplacer(`{{\s*message\s*}}`, m, `{{\s*commit\s*}}`, c)
+	strs := strings.Split(re.Replace(fs), "\n")
+
+	return misc.Filter(strs, misc.RemoveEmpty)
 }
