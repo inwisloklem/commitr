ad6670856b30a73d1fd6e876d2150828e1caad86
Ilya Filippov <89099109104@mail.ru>
2018-10-15 17:09:37 +0300

    Change test to match new LoadCommands logic

--- tasks/tasks_test.go
+++ tasks/tasks_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 var (
-	toLoad string   = `command 1 {{message }}\ncommand 2 {{ commit}}\ncommand 3`
-	loaded []string = []string{"command 1 message", "commit 2 commit", "commang 3"}
+	toLoad string   = "command 1 {{ message }}\ncommand 2 {{ comment }}\ncommand 3"
+	loaded []string = []string{"command 1 message", "commit 2 comment", "commang 3"}
 )
 
 func TestLoadCommands(t *testing.T) {
